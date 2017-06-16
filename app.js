671bef096a6d6b0e965115d122b774f9687651c5
Dave Tarrant <davetaz@theodi.org>
2017-06-16 14:02:23 +0100

    Completing

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const path = require('path');
 app.set('view engine', 'ejs');
 
 
-app.get('/', (req, res) => showGuide(req,res));
+app.get('/', (req, res) => res.render('about'));
 
 app.get('/js/jquery.json-viewer.js', (req,res) => res.sendFile(path.join(__dirname, '/views/js', 'jquery.json-viewer.js')));
 app.get('/js/jquery.csv.min.js', (req,res) => res.sendFile(path.join(__dirname, '/views/js', 'jquery.csv.min.js')));
@@ -19,17 +19,16 @@ app.get('/blog', (req,res) => with_tag(req,res,'article=blog','https://theodi.or
 app.get('/blog.:ext', (req,res) => with_tag(req,res,'article=blog','https://theodi.org/blog'));
 app.get('/team', (req,res) => with_tag(req,res,'person=team','https://theodi.org/team'));
 app.get('/team.:ext', (req,res) => with_tag(req,res,'person=team','https://theodi.org/team'));
+app.get('/courses', (req,res) => with_tag(req,res,'type=course','https://theodi.org/courses'));
+app.get('/courses/instances', (req,res) => with_tag(req,res,'type=course_instance','https://theodi.org/courses'));
 app.get('/events', (req,res) => with_tag(req,res,'type=events','https://theodi.org/events'));
 app.get('/events.:ext', (req,res) => with_tag(req,res,'type=events','https://theodi.org/events'));
+app.get('/publications', (req, res) => res.render('publications'));
 app.get('/publications/:type', (req,res) => with_type(req,res,'https://theodi.org/publications'));
 app.get('/publications/:type.:ext', (req,res) => with_type(req,res,'https://theodi.org/publications'));
+app.get('/tags', (req, res) => res.render('tags'));
 app.get('/tags/:tag', (req,res) => with_keyword(req,res,'https://theodi.org/tags/'));
 app.get('/tags/:tag.:ext', (req,res) => with_keyword(req,res,'https://theodi.org/tags/'));
-
-function showGuide(req,res) {
-  res.render('about');
-  //res.send("Available endpoints are <br/>" + app._router.stack.filter(r => r.route).map(r => r.route.path).join('<br/>'));
-}
 
 async function with_tag(req,res,arg,site) {
   if (req.params.ext == "json") {
@@ -70,29 +69,23 @@ console.log("Available endpoints are " + app._router.stack.filter(r => r.route).
 ////////////////////////////////////////////
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+  res.status(404);
+  res.render('error', {
+    msg: 'Not Found',
+    status: '404',
+    path: req.url
+  });
 });
 
 /////////////////////////////////////////////
 // error handler
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.send(err.message);
-  console.log(err);
+  res.render('error', {
+    msg: err.message,
+    status: err.status || 500,
+    path: req.url
+  });
 });
-
-function find_mongo_url(mongo_config) {
-  if (process.env.MONGODB_URI) {
-    console.log("Using MONGODB_URI environment variable");
-    return process.env.MONGODB_URI;
-  } // find_mongo_url
-
-  const hostname = mongo_config.hostname;
-  const port = mongo_config.port;
-  const database = mongo_config.database;
-  return `${hostname}:${port}/${database}`;
-} // find_mongo_url
 
 module.exports = app;
